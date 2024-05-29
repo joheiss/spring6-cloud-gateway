5537fece3b1edc344294df037ccec152b70910a4
Josef Heiss <josef.heiss@jovisco.de>
2024-05-29 13:28:00 +0200

    after section 36

---
+++ src/test/java/com/jovisco/spring6cloudgateway/Spring6CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovisco.spring6cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
