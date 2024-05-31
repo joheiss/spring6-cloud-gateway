4a2d2cdd1fe4054f66b9b9b9fd75e53d9d65a742
Josef Heiss <josef.heiss@jovisco.de>
2024-05-31 19:26:34 +0200

    after section 43 -actuator

--- src/main/java/com/jovisco/spring6cloudgateway/config/SecurityConfig.java
+++ src/main/java/com/jovisco/spring6cloudgateway/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.jovisco.spring6cloudgateway.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -10,6 +12,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
 
     @Bean 
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
